0a9f8853cfe4caef4df5ceb97662173a3c4664f0
Daniela Camacho Gomez <camachodaniela143@gmail.com>
2025-03-20 15:49:15 -0600

    feat:Create InfoCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import { InfoCard } from "@/components/InfoCard/InfoCard";
 
 export default function Home() {
   const [visibleBadges, setVisibleBadges] = useState({
@@ -9,14 +10,14 @@ export default function Home() {
     defaultClosable: true,
     secondaryClosable: true,
     destructiveClosable: true,
-  })
+  });
 
   const handleClose = (badge: string) => {
     setVisibleBadges((prevState) => ({
       ...prevState,
       [badge]: false,
-    }))
-  }
+    }));
+  };
   return (
     <>
       <div className="space-y-4">
@@ -30,41 +31,57 @@ export default function Home() {
       </div>
 
       <div className="space-y-4">
-      {/* Badges sin onClose */}
-      <div className="flex flex-wrap gap-2">
-        <Badge variant="default">Abierto</Badge>
-        <Badge variant="secondary">Cerrado</Badge>
-        <Badge variant="destructive">En Revisión</Badge>
-        <Badge variant="outline">Ingeniería en Software</Badge>
+        {/* Badges sin onClose */}
+        <div className="flex flex-wrap gap-2">
+          <Badge variant="default">Abierto</Badge>
+          <Badge variant="secondary">Cerrado</Badge>
+          <Badge variant="destructive">En Revisión</Badge>
+          <Badge variant="outline">Ingeniería en Software</Badge>
+        </div>
+
+        {/* Badges con onClose */}
+        <div className="flex flex-wrap gap-2">
+          {visibleBadges.outlineClosable && (
+            <Badge
+              variant="outline"
+              onClose={() => handleClose("outlineClosable")}
+            >
+              Ingeniería en Software
+            </Badge>
+          )}
+
+          {visibleBadges.defaultClosable && (
+            <Badge
+              variant="defaultClosable"
+              onClose={() => handleClose("defaultClosable")}
+            >
+              Default Badge with Close
+            </Badge>
+          )}
+
+          {visibleBadges.secondaryClosable && (
+            <Badge
+              variant="secondaryClosable"
+              onClose={() => handleClose("secondaryClosable")}
+            >
+              Secondary Badge with Close
+            </Badge>
+          )}
+
+          {visibleBadges.destructiveClosable && (
+            <Badge
+              variant="destructiveClosable"
+              onClose={() => handleClose("destructiveClosable")}
+            >
+              Destructive Badge with Close
+            </Badge>
+          )}
+        </div>
       </div>
-
-      {/* Badges con onClose */}
-      <div className="flex flex-wrap gap-2">
-        {visibleBadges.outlineClosable && (
-          <Badge variant="outline" onClose={() => handleClose("outlineClosable")}>
-            Ingeniería en Software
-          </Badge>
-        )}
-
-        {visibleBadges.defaultClosable && (
-          <Badge variant="defaultClosable" onClose={() => handleClose("defaultClosable")}>
-            Default Badge with Close
-          </Badge>
-        )}
-
-        {visibleBadges.secondaryClosable && (
-          <Badge variant="secondaryClosable" onClose={() => handleClose("secondaryClosable")}>
-            Secondary Badge with Close
-          </Badge>
-        )}
-
-        {visibleBadges.destructiveClosable && (
-          <Badge variant="destructiveClosable" onClose={() => handleClose("destructiveClosable")}>
-            Destructive Badge with Close
-          </Badge>
-        )}
+      <div className="flex">
+        <InfoCard avatar="https://github.com/shadcn.png" name="Jane Daw" email="Hosea28@yahoo.com" cellphone="+52 441 441 22 22"/>
+        <InfoCard avatar="https://github.com/shadcn.png" name="Jane Daw" email="Hosea28@yahoo.com" cellphone={null}/>
       </div>
-    </div>
     </>
   );
 }
