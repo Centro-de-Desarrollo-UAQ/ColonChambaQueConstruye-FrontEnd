ddf6a48b9705caf7a53ebb392f941d884f7a7e5e
GsusMR <jmoreno91@alumnos.uaq.mx>
2025-09-30 12:42:38 -0600

    Created linker and landing navbar

--- src/components/ui/header.linker.tsx
+++ src/components/ui/header.linker.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import * as React from 'react';
 import Image from 'next/image';
 import { User,Logout2} from '@solar-icons/react';
-import { Button } from "@/components/ui/button"; // si usas shadcn/ui
+import {Button} from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -12,55 +12,49 @@ import {
   DropdownMenuItem
 } from '@/components/ui/dropdown-menu';
 
-interface HeaderProps{
+interface HeaderLinkerProps{
     userIcon?: React.ReactNode;
     logOut?: React.ReactNode;
-    companyImageUrl?: string; //URL del logo de la compañía
-    companyTitle?: string; //Nombre de la compañía
+    linkermail?: string; //Obtener el mail del linker
 }
 
-export default function Header({ 
-    companyTitle='Deloitte Qro', //Obtener el nombre de la compañía
-    companyImageUrl='/Deloitte.svg', //Obtener el logo de la compañía
-    userIcon = <User className="h-5 w-5"/> , 
-    logOut = <Logout2 className='h-5 w-5'/> }: HeaderProps) {
+
+export default function HeaderLinker({linkermail = "vinculadorx@gmail.com", userIcon = <User className="h-5 w-5"/> , 
+    logOut = <Logout2 className='h-5 w-5'/>}:HeaderLinkerProps) {
   return (
     <>
         <header className='bg--accent flex items-center justify-between px-10 border-b border-zinc-200 drop-shadow-md'>
-            <div className='flex items-center gap-4 py-4'>
+             <div className='flex items-center gap-4 py-4'>
                 <Link href="/" className="text-lg font-bold">
                     <Image src="/UCQC.png" alt="Colon" width={120} height={120} className="h-10 w-28" />
                 </Link>
                 <Link href="/" className="text-lg font-bold">
                     <Image src="/ADMON24-27-1-03.png" alt="Colon" width={120} height={120} className="h-10 w-28" />
                 </Link>
-            </div>
+             </div>
             <div className="flex items-center justify-center">
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                         <Button variant="mono">
-                            <Image src={companyImageUrl} alt={companyTitle} width={32} height={32} className="h-8 w-8 rounded-full" />
-                            {companyTitle}
+                            {linkermail}
                         </Button>
                     </DropdownMenuTrigger>
 
                     <DropdownMenuContent>
                         <DropdownMenuItem onClick={() => console.log("Página de perfil")}>
                             {userIcon}
-                            Perfil {//Acceder a la página de perfil del empleador
+                            Perfil {//Acceder a la página de perfil del linker
                             }
                         </DropdownMenuItem>
                         <DropdownMenuItem onClick={() => console.log("Cerrar sesión")} variant="destructive">
                             {logOut}
-                            Cerrar sesión {//Procedimiento de cierre de sesión del empleador
+                            Cerrar sesión {//Procedimiento de cierre de sesión del linker
                             }
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
-            
         </header>
-        
     </>
-  );
+  )
 }
